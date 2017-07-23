aa32b7b96b5cfaf2bd6b41f90008b594500b071a
Shuja Shabandri <shuja@shabandri.com>
2017-07-23 07:35:44 +0400

    Sequelize

--- server/src/data/schema.js
+++ server/src/data/schema.js
@@ -2,11 +2,14 @@ import {
   GraphQLSchema as Schema,
   GraphQLObjectType as ObjectType,
 } from 'graphql';
+import sequelize from './sequelize';
+import { getSchema } from 'graphql-sequelize-crud-teselagen';
 
-import me from './queries/me';
-import news from './queries/news';
-import tracks from './queries/tracks';
+// import me from './queries/me';
+// import news from './queries/news';
+// import tracks from './queries/tracks';
 
+/*
 const schema = new Schema({
   query: new ObjectType({
     name: 'Query',
@@ -17,5 +20,7 @@ const schema = new Schema({
     },
   }),
 });
+*/
 
+const schema = getSchema(sequelize);
 export default schema;
